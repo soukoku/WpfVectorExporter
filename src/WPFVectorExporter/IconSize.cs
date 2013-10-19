0e22f30b1b5985f6e5d415d56a1aacdd7ec6f1f4
soukoku <yinchun.wang@gmail.com>
2013-10-18 22:58:38 -0400

    Initial commit.

---
+++ src/WPFVectorExporter/IconSize.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFVectorExporter
+{
+    public class IconSize
+    {
+        public static readonly IEnumerable<IconSize> TypicalSizes = new[]
+        {
+            new IconSize{ Size =16, Padding=0},            
+            new IconSize{ Size =24, Padding=1},            
+            new IconSize{ Size =32, Padding=2},    
+            new IconSize{ Size =48, Padding=3},    
+            new IconSize{ Size =64, Padding=4},    
+            new IconSize{ Size =128, Padding=4},    
+            new IconSize{ Size =256, Padding=8},    
+            new IconSize{ Size =512, Padding=16},
+        };
+
+        public int Size { get; set; }
+        public int Padding { get; set; }
+
+        public int InnerSize { get { return Size - (Padding * 2); } }
+    }
+}
---
+++ src/WPFVectorExporter/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.WindowsAPICodePack.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+
+namespace WPFVectorExporter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExport_Click(object sender, RoutedEventArgs e)
+        {
+            var dlg = GetFolderDialog();
+
+            if (dlg.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                ExportImages(dlg.FileName);
+            }
+        }
+
+        private void ExportImages(string folder)
+        {
+            var sampleSize = vectorHolder.DataContext;
+            foreach (var size in IconSize.TypicalSizes)
+            {
+                vectorHolder.DataContext = size;
+                var fileName = System.IO.Path.Combine(folder, string.Format("vector-{0}.png", size.Size));
+                WriteOutImage(size.Size, vectorHolder, fileName);
+            }
+            vectorHolder.DataContext = sampleSize;
+            Process.Start(folder);
+        }
+
+        private void WriteOutImage(int size, FrameworkElement vector, string fileName)
+        {
+            Size availableSize = new Size(size, size);
+            vector.Measure(availableSize);
+            vector.Arrange(new Rect(availableSize));
+
+            RenderTargetBitmap bitmap = new RenderTargetBitmap(size, size, 96, 96, PixelFormats.Default);
+            bitmap.Render(vector);
+
+            PngBitmapEncoder encoder = new PngBitmapEncoder();
+            encoder.Frames.Add(BitmapFrame.Create(bitmap));
+
+            using (var fs = File.Create(fileName))
+            {
+                encoder.Save(fs);
+            }
+        }
+
+        private static CommonOpenFileDialog GetFolderDialog()
+        {
+            var dlg = new CommonOpenFileDialog();
+            dlg.Title = "Select Export Location";
+            dlg.IsFolderPicker = true;
+            //dlg.InitialDirectory = currentDirectory;
+
+            dlg.AddToMostRecentlyUsedList = false;
+            dlg.AllowNonFileSystemItems = false;
+            //dlg.DefaultDirectory = currentDirectory;
+            dlg.EnsureFileExists = true;
+            dlg.EnsurePathExists = true;
+            dlg.EnsureReadOnly = false;
+            dlg.EnsureValidNames = true;
+            dlg.Multiselect = false;
+            dlg.ShowPlacesList = true;
+            return dlg;
+        }
+    }
+}
