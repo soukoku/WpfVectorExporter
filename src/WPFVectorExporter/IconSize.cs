a8e4700b47d82104e68dcea170855fbd940f94c1
soukoku <yinchun.wang@gmail.com>
2013-10-26 11:58:29 -0400

    Changed to framework4 and updated sample sizes.

--- src/WPFVectorExporter/IconSize.cs
+++ src/WPFVectorExporter/IconSize.cs
@@ -1,13 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace WPFVectorExporter
 {
     public class IconSize
     {
+        public static IconSize Sample { get { return TypicalSizes.Last(); } }
+
         public static readonly IEnumerable<IconSize> TypicalSizes = new[]
         {
             new IconSize{ Size =16, Padding=0},            
@@ -15,13 +14,13 @@ namespace WPFVectorExporter
             new IconSize{ Size =32, Padding=2},    
             new IconSize{ Size =48, Padding=3},    
             new IconSize{ Size =64, Padding=4},    
-            new IconSize{ Size =128, Padding=4},    
-            new IconSize{ Size =256, Padding=8},    
-            new IconSize{ Size =512, Padding=16},
+            new IconSize{ Size =128, Padding=8},    
+            new IconSize{ Size =256, Padding=16},    
+            new IconSize{ Size =512, Padding=32},
         };
 
-        public int Size { get; set; }
-        public int Padding { get; set; }
+        public int Size { get; private set; }
+        public int Padding { get; private set; }
 
         public int InnerSize { get { return Size - (Padding * 2); } }
     }
